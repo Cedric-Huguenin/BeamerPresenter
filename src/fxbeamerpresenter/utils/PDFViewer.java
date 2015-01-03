5d1af58f3a5576cddbc96fc8c2754dacf5198626
Cédric Huguenin <cedric.huguenin@telecomnancy.eu>
2015-01-03 22:58:20 +0100

    [Fix] Several SumatraPDF instances were launched

--- src/fxbeamerpresenter/utils/PDFViewer.java
+++ src/fxbeamerpresenter/utils/PDFViewer.java
@@ -13,30 +13,25 @@ import java.io.IOException;
  * @author Ced
  */
 public class PDFViewer {
-    
-    public PDFViewer() {
-        
-    }
-    
-    public void viewPDF(PDFModel pdfModel) {
-        viewPDFUtils(pdfModel.getPdfPath(), pdfModel.getPage());
-        viewPDFUtils(pdfModel.getNotesPath(), pdfModel.getPage());
-    }
-    
-    private void viewPDFUtils(String path, int page) {
-        try {
-            ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "sumatrapdf "+path+" -reuse-instance -page " + page);
-            builder.redirectErrorStream(true);
-            final Process process = builder.start();
 
-            try {
-                Process p = builder.start();
-            } catch (IOException e) {
-                System.err.println(e);
-            }
-        } catch (IOException e) {
+	public PDFViewer() {
 
-        }
-    }
-    
+	}
+
+	public void viewPDF(PDFModel pdfModel) {
+		viewPDFUtils(pdfModel.getPdfPath(), pdfModel.getPage());
+		viewPDFUtils(pdfModel.getNotesPath(), pdfModel.getPage());
+	}
+
+	private void viewPDFUtils(String path, int page) {
+		ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "sumatrapdf " + path + " -reuse-instance -page " + page);
+		builder.redirectErrorStream(true);
+
+		try {
+			Process p = builder.start();
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+
 }
